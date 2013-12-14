e62248038b4f0db5988383aa7689c9fd4c258d01
Ian Truslove <ian.truslove@sonian.net>
2013-12-13 17:08:38 -0700

    Unwinding the default line length

--- src/cowsay/text_formatter.clj
+++ src/cowsay/text_formatter.clj
@@ -1,9 +1,9 @@
 (ns cowsay.text-formatter
   (:require [clojure.string :refer [join]]))
 
-(def line-length "Maximum single-line length of a cowsay" 39)
+(def default-line-length "Maximum single-line length of a cowsay" 39)
 
-(defn cowformat [s]
+(defn cowformat [s line-length]
   "Line wraps the string s appropriately for cowsaying.
   TODO:  give a crap about splitting on word boundaries."
   (loop [lines []
--- test/cowsay/text_formatter_test.clj
+++ test/cowsay/text_formatter_test.clj
@@ -4,13 +4,13 @@
 
 (deftest test-formatter
   (testing "Short phrases aren't wrapped"
-    (is (= ["no line wraps"] (cowformat "no line wraps"))))
+    (is (= ["no line wraps"] (cowformat "no line wraps" default-line-length))))
 
   (testing "39-letter words fit in whole"
     (let [long-word (apply str (repeat 39 "a"))]
-      (is (= [long-word] (cowformat long-word)))))
+      (is (= [long-word] (cowformat long-word default-line-length)))))
 
   (testing "40+ letter words are split onto new lines"
     (let [long-word (apply str (repeat 39 "a"))]
       (is (= [long-word long-word]
-             (cowformat (str long-word long-word)))))))
+             (cowformat (str long-word long-word) default-line-length))))))
